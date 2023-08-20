dd6da4d5dba49e06d6e6a01ac76aaf5e3cafb96b
karthikjohnn <karthickyamkk2001@gmail.com>
2023-08-20 14:35:14 +0530

    Animation taiwind config file images updated

--- index.js
+++ index.js
@@ -4,40 +4,49 @@ function activeMenu() {
     menuBar.classList.toggle("translate-x-0")
 
 }
-document.querySelector("#menuBar").addEventListener("click",()=>{
+document.querySelector("#menuBar").addEventListener("click", () => {
     document.querySelector("#menuBar").classList.remove("translate-x-0")
 })
-
 
 
 // stickybar small devices
 document.addEventListener("scroll", () => {
     var pageOffset = window.pageYOffset
-    if (pageOffset > 150) {
-        document.querySelector("#stickybar").classList.add("shadow-lg")
-        if (pageOffset > 2500) {
-            document.querySelector("#stickybar").classList.add("!right-0")
-        } else {
-            document.querySelector("#stickybar").classList.remove("!right-0")
-        }
-    } else {
-        document.querySelector("#stickybar").classList.remove("shadow-lg")
-    }
 
-})
+    if (pageOffset > 2500) {
+        document.querySelector("#stickybar").classList.add("!right-0")
+    }
+}
+)
+
+function stickySmallClose() {
+    document.querySelector("#stickybar").classList.toggle("!right-[-54px]")
+    console.log("btn wrk");
+}
+
 
 // stickybar large devices
 document.addEventListener("scroll", () => {
     var pageOffset = window.pageYOffset
     if (pageOffset > 1800) {
-        document.querySelector("#stickyBarLarge").classList.add("!translate-y-0")
+        document.querySelector("#stickyBarLarge").classList.add("bottom-[-10px]")
     }
 })
-// stickybar remove from DOM 
 function stickyLargeClose() {
-    document.querySelector("#stickyBarLarge").remove()
+    // console.log(document.querySelector("#stickyBarLarge").clientHeight)
+    // document.querySelector("#stickyBarLarge").remove()
+    const stickyDiv = document.querySelector("#stickyBarLarge").classList.length
+    if (stickyDiv == 13) {
+        document.querySelector("#stickyCloseBtn").childNodes[1].classList.add("hidden")
+        document.querySelector("#stickyCloseBtn").childNodes[3].classList.remove("hidden")
+    } else if (stickyDiv == 14) {
+        document.querySelector("#stickyCloseBtn").childNodes[1].classList.remove("hidden")
+        document.querySelector("#stickyCloseBtn").childNodes[3].classList.add("hidden")
+    }
+    document.querySelector("#stickyBarLarge").classList.toggle("bottom-[-100px]")
 }
 
+// M4.5 15.75l7.5-7.5 7.5 7.5
 
 
 // show and hidden
@@ -58,11 +67,13 @@ hideEl.forEach((el) => observe.observe(el))
 // preloader 
 var loader = document.getElementById("preloader")
 const showPageElm = document.getElementById("showPage")
-window.addEventListener("load",()=>{
-    setTimeout(showPage,1500)
+window.addEventListener("load", () => {
+    if (showPageElm.classList == "hidden") {
+        setTimeout(showPage, 1500)
+    }
 })
 
-const showPage =()=>{
-    loader.style.display="none";
+const showPage = () => {
+    loader.classList.add("!hidden")
     showPageElm.classList.add("!block")
 }
